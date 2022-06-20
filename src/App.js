a8d73624479dc209e467288148787c7c5c3e2b78
Dejan Conkic <conkicdejan@gmail.com>
2022-06-20 21:39:46 +0200

    Zadatak 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const animals = [
+        { vrsta: "Pas", ime: "Dzeki", datumRodjenja: new Date('01/06/2018') },
+        { vrsta: "Macka", ime: "Lili", datumRodjenja: new Date('01/06/2018') },
+        { vrsta: "Ribica", ime: "Zlatko", datumRodjenja: new Date('01/06/2018') },
+        { vrsta: "Konj", ime: "Misa", datumRodjenja: new Date('01/06/2018') },
+        { vrsta: "Curka", ime: "Gabl", datumRodjenja: new Date('01/06/2018') },
+    ];
+
+    return (
+        <div>
+            <ul>
+                {animals.map((animal, index) => (
+                    <li key={index}>{animal.vrsta} {animal.ime} {animal.datumRodjenja}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
