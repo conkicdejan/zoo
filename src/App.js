93e320f11698fb00b1671d413aa69314c64ebf23
Dejan Conkic <conkicdejan@gmail.com>
2022-06-21 17:43:19 +0200

    Zadatak 2

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
 import "./App.css";
 
 function App() {
-    const animals = [
-        { vrsta: "Pas", ime: "Dzeki", datumRodjenja: new Date('01/06/2018') },
-        { vrsta: "Macka", ime: "Lili", datumRodjenja: new Date('01/06/2018') },
-        { vrsta: "Ribica", ime: "Zlatko", datumRodjenja: new Date('01/06/2018') },
-        { vrsta: "Konj", ime: "Misa", datumRodjenja: new Date('01/06/2018') },
-        { vrsta: "Curka", ime: "Gabl", datumRodjenja: new Date('01/06/2018') },
-    ];
+  const animals = [
+    { vrsta: "Pas", ime: "Dzeki", datumRodjenja: new Date("01/06/2018") },
+    { vrsta: "Macka", ime: "Lili", datumRodjenja: null },
+    { vrsta: "Ribica", ime: "Zlatko" },
+    { vrsta: "Konj", ime: "Misa", datumRodjenja: '' },
+    { vrsta: "Curka", ime: "Gabl", datumRodjenja: new Date("01/06/2018") },
+  ];
 
-    return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th width='150px'>Vrsta</th>
-                        <th width='150px'>Ime</th>
-                        <th width='150px'>Datum rodjenja</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {animals.map((animal, index) => (
-                        <tr key={index}>
-                            <td>{animal.vrsta}</td>
-                            <td>{animal.ime}</td>
-                            <td>{animal.datumRodjenja.toLocaleDateString()}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+  const dateToString = (date) =>
+    date ? date.toLocaleDateString() : "Nepoznat";
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th width="150px">Vrsta</th>
+            <th width="150px">Ime</th>
+            <th width="150px">Datum rodjenja</th>
+          </tr>
+        </thead>
+        <tbody>
+          {animals.map((animal, index) => (
+            <tr key={index}>
+              <td>{animal.vrsta}</td>
+              <td>{animal.ime}</td>
+              <td>{dateToString(animal.datumRodjenja)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
