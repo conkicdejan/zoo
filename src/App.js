ff086b3b2a5ab08049bb5997e31030055c6bb60b
Dejan Conkic <conkicdejan@gmail.com>
2022-06-21 17:54:58 +0200

    Zadatak 3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  const animals = [
+  const [animals, setAnimals] = useState([
     { vrsta: "Pas", ime: "Dzeki", datumRodjenja: new Date("01/06/2018") },
     { vrsta: "Macka", ime: "Lili", datumRodjenja: null },
     { vrsta: "Ribica", ime: "Zlatko" },
-    { vrsta: "Konj", ime: "Misa", datumRodjenja: '' },
+    { vrsta: "Konj", ime: "Misa", datumRodjenja: "" },
     { vrsta: "Curka", ime: "Gabl", datumRodjenja: new Date("01/06/2018") },
-  ];
+  ]);
 
   const dateToString = (date) =>
     date ? date.toLocaleDateString() : "Nepoznat";
+
+  const handleRemove = (index) => {
+    const newAnimals = animals.filter((animal, i) => i !== index);
+    setAnimals([...newAnimals]);
+  };
 
   return (
     <div>
@@ -20,6 +26,7 @@ function App() {
             <th width="150px">Vrsta</th>
             <th width="150px">Ime</th>
             <th width="150px">Datum rodjenja</th>
+            <th width="150px">Akcije</th>
           </tr>
         </thead>
         <tbody>
@@ -28,6 +35,9 @@ function App() {
               <td>{animal.vrsta}</td>
               <td>{animal.ime}</td>
               <td>{dateToString(animal.datumRodjenja)}</td>
+              <td>
+                <button onClick={() => handleRemove(index)}>Remove</button>
+              </td>
             </tr>
           ))}
         </tbody>
